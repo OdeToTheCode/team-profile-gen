a2a01b0f9fb1aa2829efd3c35a19e7f53ab99ce2
OdeToTheCode <119718185+OdeToTheCode@users.noreply.github.com>
2023-01-31 01:34:11 -0600

    fixed a couple of things

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ let complete = false
 
 async function start() {
   // Ask the user for Manager information and wait for the response
-  const managerData = await inquirer.prompt(Manager.managerPrompts);
+  const managerData = await inquirer.prompt(Manager.managerPrompts)
 
   // Store the Manager information
-  teamMembers.push(Manager.collectTeamInfo(managerData));
+  teamMembers.push(Manager.collectTeamInfo(managerData))
 
   // Start adding more team members
-  await addTeamMember();
+  await addTeamMember()
 }
 
 async function addTeamMember() {
@@ -28,37 +28,36 @@ async function addTeamMember() {
       name: 'teamSelect',
       choices: ['engineer', 'intern', 'team complete'],
       message: 'Which type of team member would you like to add?'
-    }]);
+    }])
 
     switch (teamType.teamSelect) {
       case 'engineer':
         // Ask for Engineer information and store it
-        const engineerData = await inquirer.prompt(Engineer.engineerPrompts);
-        teamMembers.push(Engineer.collectTeamInfo(engineerData));
-        break;
+        const engineerData = await inquirer.prompt(Engineer.engineerPrompts)
+        teamMembers.push(Engineer.collectTeamInfo(engineerData))
+        break
 
       case 'intern':
         // Ask for Intern information and store it
-        const internData = await inquirer.prompt(Intern.internPrompts);
-        teamMembers.push(Intern.collectTeamInfo(internData));
-        break;
+        const internData = await inquirer.prompt(Intern.internPrompts)
+        teamMembers.push(Intern.collectTeamInfo(internData))
+        break
 
       case 'team complete':
         // Set the complete flag and stop asking for more team members
-        complete = true;
+        complete = true
     }
 
     // Keep asking for more team members
-    await addTeamMember();
+    await addTeamMember()
   } else {
     // Generate the HTML once all team members have been added
-    generateHTML();
+    generateHTML()
   }
 }
 
-function generateHTML() { // dynamiclly generate the html using a template literal
-  let html = `
-  <!DOCTYPE html>
+function generateHTML() {
+  let html = ` <!DOCTYPE html>
   <html lang="en">
   <head>
       <meta charset="UTF-8">
@@ -67,52 +66,63 @@ function generateHTML() { // dynamiclly generate the html using a template liter
       <title>My Team</title>
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
   </head>
-
   <body>
-
-      <header class="container-fluid text-center text-light bg-danger py-5">
-          <h1>My Team</h1>
-       </header>
-
-      <main class="container-fluid pt-5 d-flex justify-content-center flex-wrap">`
-
-      teamMembers.forEach(teamMember => {
-        html += `
-      <div class="card col-2 mx-2">
-      <div class="card-header bg-primary text-light">
-          <h4 class="card-title">${teamMember.getName()}</h4>
-          <h4 class="card-text">${teamMember.getRole()}</h4>
-      </div>
-
-      <div class="card-body my-3">
-          <ul class="list-group">
-              <li class="list-group-item">ID: ${teamMember.getId()}</li>
-              <li class="list-group-item">Email: <a href="mailto:${teamMember.getEmail()}">${teamMember.getEmail()}</a></li>
-              <li class="list-group-item">`
-      
-      switch (teamMember.getRole()) {
-          case 'Manager':
-              html += `Office Number: ${teamMember.officeNumber} </li>`
-              break
-          case 'Engineer':
-              html += `Github: <a href="https://github.com/${teamMember.github}" target="_blank">${teamMember.github}</a></li>`
-              break
-          case 'Intern':
-              html += `School: ${teamMember.school}</li>`
-              break
-      }
-      html += `
-              </ul>
+      <header class="text-center text-light bg-danger py-5">
+          <div class="container-fluid">
+              <h1>My Team</h1>
           </div>
+      </header>
+      <main class="d-flex justify-content-center flex-wrap pt-5">`
+
+  teamMembers.forEach(member => {
+    html += `
+      <div class="card mx-2 col-2">
+        <div class="card-header bg-primary text-light">
+            <h4 class="card-title">${member.getName()}</h4>
+            <h4 class="card-text">${member.getRole()}</h4>
+        </div>
+        <div class="card-body my-3">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${member.getId()}</li>
+                <li class="list-group-item">Email: 
+                    <a href="mailto:${member.getEmail()}">
+                      ${member.getEmail()}
+                    </a>
+                </li>`
+    switch (member.getRole()) {
+      case 'Manager':
+        html += `<li class="list-group-item">Office Number: ${member.officeNumber}</li>`
+        break
+      case 'Engineer':
+        html += `<li class="list-group-item">Github: 
+                  <a href="https://github.com/${member.github}" target="_blank">
+                    ${member.github}
+                  </a>
+                </li>`
+        break
+      case 'Intern':
+        html += `<li class="list-group-item">School: ${member.school}</li>`
+        break
+    }
+    html += `
+            </ul>
+        </div>
       </div>`
   })
+
   html += `
       </main>
   </body>
   </html>`
 
-  fs.writeFile('./dist/index.html', html, err => err ? console.log(err) : console.log('Your Team page has been generated!!'))
-
+  fs.writeFile('./dist/index.html', html, function(err) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log('Team page created successfully!')
+    }
+  })
 }
+
 
 start()
